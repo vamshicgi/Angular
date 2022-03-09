9dc1a20d010d29b737bc184951c93474913f1951
CGIVAMSHI <cgi.vamshi@gmail.com>
2022-03-09 23:50:57 +0530

    installing primeng,bootstrap,tslib and adding routing

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -2,13 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { EmployeeComponent } from './pages/admin/employee/employee.component';
+import { TeamComponent } from './pages/admin/team/team.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './pages/admin/admin.component';
+import { LocationComponent } from './pages/admin/location/location.component';
+
+
+const routes: Routes = [
+  {
+   path:'admin',
+   component:AdminComponent,
+   children: [
+    { path:'location',component:LocationComponent },
+    { path:'team',component:TeamComponent },
+    { path:'employee',component:EmployeeComponent }
+    ]
+}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    DashboardComponent,
+    EmployeeComponent,
+    TeamComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
